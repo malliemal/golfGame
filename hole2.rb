19e7a1f4feb2538e6b810f62041e288e20bdfcd6
Mallory Linton <mallorylinton@gmail.com>
2018-05-21 23:34:50 -0600

    update hole2.rb

--- hole2.rb
+++ hole2.rb
@@ -1,12 +1,11 @@
 def rps()
-    puts "Enter a choice- R for rock, P for paper, S 
-    for scissors"
+    puts "Enter R-rock, P-paper, S-scissors"
     u = gets.strip
     c = ["R", "P", "S"]
     d = c.sample
    
     puts " #{u} #{d}"
-    #R < P P < S S<R
+    R < P P < S S<R
     
   
    if (u == "R" && d == "S" || u=="P" && d=="R"||u=="S" && d=="P")
