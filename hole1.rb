3aa07bebd5cf59cca568217b6e532cb9741db9e5
Mallory Linton <mallorylinton@gmail.com>
2018-05-21 23:49:14 -0600

    update to hole1 and hole3

--- hole1.rb
+++ hole1.rb
@@ -1,11 +1,11 @@
 def sum
     puts "Number?"
-    nums=gets.chomp
+    num=gets.chomp
     sum=0
-    nums=nums.split
-    nums.each do |number|
+    num=num.split
+    num.each do |number|
     sum=sum + number.to_i 
     end
     return sum
-  end
-  puts sum
+end
+puts sum
